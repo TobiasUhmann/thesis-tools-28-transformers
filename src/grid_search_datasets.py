a06209f810fc1f17dc7360d7bd8e41019d01a66c
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-03-25 16:49:59 +0100

    Generalize batch size search to BASE/OWER && Specify config within script

--- src/grid_search_datasets.py
+++ src/grid_search_datasets.py
@@ -1,36 +1,50 @@
 import logging
+from argparse import Namespace
 from pprint import pformat
 
 from train_base_bert import train_base_bert
 from train_ower_bert import train_ower_bert
-from util.util import get_default_args
 
 
 def main():
     logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=logging.INFO)
 
-    args = get_default_args()
+    ## Specify config
+
+    args = Namespace()
+
+    # args.ower_dir
+    args.class_count = 100
+    # args.sent_count
+
+    # args.batch_size
+    args.device = 'cuda'
+    args.epoch_count = 20
+    # args.log_dir
+    args.log_steps = True
+    args.lr = 1e-5
+    args.sent_len = 64
 
     # Datasets with respective sentence counts and appropriate batch sizes.
     # Start out with batch sizes that work on a GTX 1080 Ti with 11GB RAM.
-    # [[dataset, sent count, batch size]]
-    dataset_choices = [
-        ['ower-v4-cde-cde-100-1', 1, 256],
-        ['ower-v4-cde-irt-100-1', 1, 256],
-        ['ower-v4-cde-irt-100-5', 5, 64],
-        ['ower-v4-cde-irt-100-15', 15, 16],
-        ['ower-v4-cde-irt-100-30', 30, 16],
-        ['ower-v4-fb-irt-100-1', 1, 256],
-        ['ower-v4-fb-irt-100-5', 5, 64],
-        ['ower-v4-fb-irt-100-15', 15, 16],
-        ['ower-v4-fb-irt-100-30', 30, 16],
-        ['ower-v4-fb-owe-100-1', 1, 512]
+    # [[(dataset, sent count), BASE batch size, OWER batch size]]
+    dataset_model_choices = [
+        [('ower-v4-cde-cde-100-1', 1), 256, 256],
+        [('ower-v4-cde-irt-100-1', 1), 256, 256],
+        [('ower-v4-cde-irt-100-5', 5), 64, 64],
+        [('ower-v4-cde-irt-100-15', 15), 16, 16],
+        [('ower-v4-cde-irt-100-30', 30), 16, 16],
+        [('ower-v4-fb-irt-100-1', 1), 256, 256],
+        [('ower-v4-fb-irt-100-5', 5), 64, 64],
+        [('ower-v4-fb-irt-100-15', 15), 16, 16],
+        [('ower-v4-fb-irt-100-30', 30), 16, 16],
+        [('ower-v4-fb-owe-100-1', 1), 512, 512]
     ]
 
-    # Try batches sizes. Decrease if graphics RAM is not sufficient until it fits.
-
-    for dataset_choice in dataset_choices:
-        dataset, sent_count, batch_size = dataset_choice
+    ## Try batches sizes. Decrease if graphics RAM is not sufficient until it fits.
+    
+    for dataset_model_choice in dataset_model_choices:
+        (dataset, sent_count), base_batch_size, ower_batch_size = dataset_model_choice
 
         args.ower_dir = f'data/ower/{dataset}'
         args.sent_count = sent_count
@@ -38,35 +52,60 @@ def main():
         args.try_batch_size = True
 
         while True:
-            args.batch_size = batch_size
+            args.batch_size = base_batch_size
 
             try:
-                logging.info(f'Try batch size {batch_size} for dataset {dataset}.')
+                logging.info(f'Try batch size {base_batch_size} for dataset {dataset} and model BASE-BERT.')
                 train_base_bert(args)
 
-                logging.info(f'Works. Use batch size {batch_size} for dataset {dataset}.')
+                logging.info(f'Works. Use batch size {base_batch_size} for dataset {dataset} and model BASE-BERT.')
                 break
 
             except RuntimeError:
-                logging.warning(f'Batch size {batch_size} too large for dataset {dataset}.'
-                                f' Half batch size to {batch_size // 2}.')
+                logging.warning(f'Batch size {base_batch_size} too large for dataset {dataset} and model BASE-BERT.'
+                                f' Half batch size to {base_batch_size // 2}.')
 
-                batch_size //= 2
-                dataset_choice[2] = batch_size
+                base_batch_size //= 2
+                dataset_model_choice[2] = base_batch_size
 
-    logging.info(f'\n{pformat(dataset_choices)}')
+        while True:
+            args.batch_size = ower_batch_size
 
-    # for i in range(3):
-    #
-    #     for dataset, sent_count in dataset_choices:
-    #         args.ower_dir = f'data/ower/{dataset}'
-    #         args.sent_count = sent_count
-    #
-    #         args.log_dir = f'runs/datasets_base-bert_{dataset}_{i}'
-    #         train_base_bert(args)
-    #
-    #         args.log_dir = f'runs/datasets_ower-bert_{dataset}_{i}'
-    #         train_ower_bert(args)
+            try:
+                logging.info(f'Try batch size {ower_batch_size} for dataset {dataset} and model OWER-BERT.')
+                train_ower_bert(args)
+
+                logging.info(f'Works. Use batch size {ower_batch_size} for dataset {dataset} and model OWER-BERT.')
+                break
+
+            except RuntimeError:
+                logging.warning(f'Batch size {ower_batch_size} too large for dataset {dataset} and model OWER-BERT.'
+                                f' Half batch size to {ower_batch_size // 2}.')
+
+                ower_batch_size //= 2
+                dataset_model_choice[3] = ower_batch_size
+
+    ## Log determined batch sizes
+
+    logging.info('')
+    logging.info(f'dataset_model_choices =\n'
+                 f'{pformat(dataset_model_choices)}')
+
+    ## Perform grid search
+
+    for i in range(3):
+
+        for (dataset, sent_count), base_batch_size, ower_batch_size in dataset_model_choices:
+            args.ower_dir = f'data/ower/{dataset}'
+            args.sent_count = sent_count
+
+            args.batch_size = base_batch_size
+            args.log_dir = f'runs/datasets_base-bert_{dataset}_{i}'
+            train_base_bert(args)
+
+            args.batch_size = ower_batch_size
+            args.log_dir = f'runs/datasets_ower-bert_{dataset}_{i}'
+            train_ower_bert(args)
 
 
 if __name__ == '__main__':
--- src/util/util.py
+++ src/util/util.py
@@ -72,30 +72,6 @@ def parse_args():
     return args
 
 
-def get_default_args():
-    args = Namespace()
-
-    # Input data
-    args.ower_dir = 'data/ower/ower-v4-fb-irt-100-5/'
-    args.class_count = 100
-    args.sent_count = 5
-
-    # Pre-processing
-    args.sent_len = 64
-
-    # Training
-    args.batch_size = 4
-    args.device = 'cuda'
-    args.epoch_count = 20
-    args.lr = 1e-5
-
-    # Logging
-    args.log_dir = None
-    args.log_steps = True
-
-    return args
-
-
 def log_class_metrics(data: Dict, writer: SummaryWriter, x: int, class_count: int) -> None:
     """
     Calculate class-wise metrics and log metrics of most/least common metrics to Tensorboard
