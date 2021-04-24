b8d06b0e363386aa99ac7b3a21103b37f91f40ef
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-04-24 22:46:50 +0200

    Make scripts executable

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,6 +6,9 @@ nohup python src/train.py \
   100 \
   5 \
   data/power/split-v2/cde-100/ \
-  data/power/texter-v2/cde-irt-5-marked.pkl \
-  data/power/eval-v1/cde-irt-5-marked.yml \
+  data/power/texter-v2/cde-irt-5-marked_power.pkl \
+  data/power/eval-v1/cde-irt-5-marked_power.yml \
+  --epoch-count 2 \
+  --model power \
+  --test \
 > logs/train_$(date +'%Y-%m-%d_%H-%M-%S').stdout &
